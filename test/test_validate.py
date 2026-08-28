296ce38031522dc75455c15849c31f4dc900464b
Br-vno <91857208+Br-vno@users.noreply.github.com>
2026-08-28 15:32:41 -0600

    Update test_validate.py

--- test/test_validate.py
+++ test/test_validate.py
@@ -1,1 +1,18 @@
+import pytest
 
+from src.validate import file_validate, FileStructureError
+
+def test_valid_csv():
+    file_validate("data/titanic.csv")
+
+def test_no_file_path():
+    with pytest.raises(FileStructureError):
+        file_validate("")
+
+def test_file_does_not_exist():
+    with pytest.raises(FileStructureError):
+        file_validate("does_not_exist.csv")
+
+def test_wrong_file_extension():
+    with pytest.raises(FilestructureError):
+        file_validate("test.txt)
