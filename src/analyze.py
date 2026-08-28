6fd713991f4580687084fe9cb89e6bcd0fd1e6ff
Br-vno <91857208+Br-vno@users.noreply.github.com>
2026-08-28 17:04:06 -0600

    Update analyze.py

--- src/analyze.py
+++ src/analyze.py
@@ -1,1 +1,27 @@
+import pandas as pd
 
+def analyze_data(dataframe):
+  rows = dataframe.shape[0]
+  columns = dataframe.shape[1]
+  
+  tot_missing_vals = dataframe.isnull().sum()
+  
+  numerical_columns = dataframe.select_dtypes(
+    include="number"
+  ).columns.tolist()
+  
+  categorical_columns = dataframe.select_dtypes(
+    include="object"
+  ).columns.tolist()
+  
+  descriptive_stats = dataframe.describe()
+  return {
+    "Rows" : rows,
+    "Columns" : columns,
+    "Total Missing Values" : tot_missing_vals,
+    "Numerical Columns" : numerical_columns,
+    "Categorical Columns" : categorical_columns,
+    "descriptive_statistics" : descriptive_statistics
+  }
+    
+  
