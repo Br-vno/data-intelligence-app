8e0a3d69db034718a1f59165bded24a0016baa94
Br-vno <91857208+Br-vno@users.noreply.github.com>
2026-08-28 15:20:39 -0600

    Update file validation

--- src/validated.py
+++ src/validated.py
@@ -1,1 +1,30 @@
+from pathlib import path
 
+class FileStructureError(Exception):
+  pass
+
+def file_validate(filename):
+    file= path(filename)
+    
+    if not filename:
+      raise FileStructureError(
+        "No file path provided. Please enter a file path."
+      )
+      
+    if not file.is_file():
+      raise FileStructureError(
+        "File does not exist. Please enter a different file."
+      )
+
+    if file.suffix.lower() != "csv":
+      raise FileStructureError(
+        "Invalid file type. Please enter a csv file"
+			)
+			
+  try:
+    with open(file, r):
+      pass
+  except PermissionError:
+    raise PermissionError(
+      "File cannot be accepted. Check the file permissions."
+    )
