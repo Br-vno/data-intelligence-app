c8ee3a9c1f11b55b11f9106d439d39f96ede022b
Br-vno <91857208+Br-vno@users.noreply.github.com>
2026-08-29 13:48:20 -0600

    add create_visualizations function

--- src/visualize.py
+++ src/visualize.py
@@ -1,1 +1,19 @@
+import matplotlib.pyplot as plt
 
+def create_visualizations(data):
+    missing_values = data["missing_values"]
+    numerical_data = data["numerical_data"]
+
+    
+    missing_values.plot(kind="bar")
+    plt.title("Missing Values by Column")
+    plt.xlabel("Column")
+    plt.ylabel("Number of Missing Values")
+    plt.tight_layout()
+    plt.savefig("missing_values.png")
+    plt.close()
+
+    numerical_data.hist()
+    plt.tight_layout()
+    plt.savefig("numerical_distributions.png")
+    plt.close()
