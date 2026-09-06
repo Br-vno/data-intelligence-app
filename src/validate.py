f502887d82da732617ad0cec540d47103cefc82b
Br-vno <91857208+Br-vno@users.noreply.github.com>
2026-09-06 13:27:46 -0600

    Update validate.py

--- src/validate.py
+++ src/validate.py
@@ -1,43 +1,49 @@
-from pathlib import path
+from pathlib import Path
 import pandas as pd
 
-
 class FileStructureError(Exception):
-  pass
+    pass
+
+
 class DatasetValidationError(Exception):
-	pass
+    pass
 
 
 def file_validate(filename):
-    file= path(filename)
-    
-    if not filename:
-      raise FileStructureError(
-        "No file path provided. Please enter a file path."
-      )
-      
-    if not file.is_file():
-      raise FileStructureError(
-        "File does not exist. Please enter a different file."
-      )
 
-    if file.suffix.lower() != "csv":
-      raise FileStructureError(
-        "Invalid file type. Please enter a csv file"
-			)
-			
-  try:
-    with open(file, r):
-      pass
-  except PermissionError:
-    raise PermissionError(
-      "File cannot be accepted. Check the file permissions."
-    )
+    if not filename:
+        raise FileStructureError(
+            "No file path provided. Please enter a file path."
+        )
+
+    file = Path(filename)
+
+    if not file.is_file():
+        raise FileStructureError(
+            "File does not exist. Please enter a different file."
+        )
+
+    if file.suffix.lower() != ".csv":
+        raise FileStructureError(
+            "Invalid file type. Please enter a csv file"
+        )
+
+    try:
+        with open(file, "r"):
+            pass
+    except PermissionError:
+        raise PermissionError(
+            "File cannot be accepted. Check the file permissions."
+        )
 
 
 def dataset_validate(dataframe):
-	if dataframe.shape[0] == 0:
-		raise DatasetValidationError("Dataset contains no rows.")
-	elif dataframe.shape[1] == 0:
-		raise DatasetValidationError("Dataset contains no columns.")
+    if dataframe.shape[0] == 0:
+        raise DatasetValidationError(
+            "Dataset contains no rows."
+        )
+    elif dataframe.shape[1] == 0:
+        raise DatasetValidationError(
+            "Dataset contains no columns."
+        )
 
