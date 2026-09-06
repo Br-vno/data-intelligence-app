05cc2c0d5808603a3df4b2571481c183e2539100
Br-vno <91857208+Br-vno@users.noreply.github.com>
2026-09-06 14:30:16 -0600

    Update test_load.py

--- test/test_load.py
+++ test/test_load.py
@@ -1,1 +1,21 @@
+import pandas as pd
+import pytest
+
+from src.load import load_data
+
+
+def test_load_data():
+  dataframe = load_data("data/titanic.csv")
+  
+  assert isinstance(dataframe, pd)
+
+
+def test_data_loaded_correctly():
+  dataframe = load_data("data/titanic.csv")
+  
+  assert len(dataframe) == 891
+  assert len(dataframe.columns) == 15
+  
+
+
 
